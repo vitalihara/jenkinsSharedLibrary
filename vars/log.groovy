69b798a2ae0399c0fe6262dbc88e8f3fad9b2a50
vitali.hara <vitalygara@gmail.com>
2020-02-03 23:26:48 +0300

    init

---
+++ vars/log.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+// vars/log.groovy
+
+
+def call(params){
+    assert params.level
+    assert params.msg
+
+    def level = params.level
+    def msg = params.msg
+
+    println "${level.toUpperCase()}: ${msg}."
+}
