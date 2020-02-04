6056cd0a67e5cbe318844974df0bf7f68bc20a1c
vitali.hara <vitalygara@gmail.com>
2020-02-04 14:13:50 +0300

    test 2

--- vars/log.groovy
+++ vars/log.groovy
@@ -9,6 +9,6 @@ def call(params) {
     def level = params.level
     def msg = params.msg
 
-    println "${env.JOB_NAME} ${level.toUpperCase()}: ${msg}."
+    println "${env.JOB_NAME} - ${env.BUILD_NUMBER} -  ${level.toUpperCase()}: ${msg}."
 
 }
